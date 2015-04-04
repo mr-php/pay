aad8cf30786fc48f5214576b9516639430cb4d45
cornernote <cornernote@gmail.com>
2015-04-05 00:45:39 +1030

    payment gateway

--- web/index.php
+++ web/index.php
@@ -1,22 +1,37 @@
 <?php
-
 require('../vendor/autoload.php');
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
-$app['debug'] = true;
+//$app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
 ));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+$app->get('/', function(Request $request) use($app) {
+    return $app['twig']->render('index.html', array(
+        'amount' => $request->get('amount'),
+        'description' => $request->get('description'),
+    ));
+});
+$app->get('/success/', function(Request $request) use($app) {
+    return $app['twig']->render('success.html');
+});
+$app->get('/declined/', function(Request $request) use($app) {
+    return $app['twig']->render('declined.html');
+});
+$app->post('/', function(Request $request) use($app) {
+    \Stripe\Stripe::setApiKey("sk_test_rWbC61qxrGhJ7JSrKSpUvvwS");
+    try {
+        $charge = \Stripe\Charge::create(array(
+            'amount' => ceil($request->get('amount') * 100),
+            'currency' => 'aud',
+            'source' => $request->get('token'),
+            'description' => $request->get('description'),
+        ));
+        return $app->redirect('success/');
+    } catch(Exception $e) {}
+    return $app->redirect('declined/');
 });
 
 $app->run();
-
-?>
