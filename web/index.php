cc80ec2651151c836d7b4e3915b6f95fe13043d2
cornernote <cornernote@gmail.com>
2015-04-05 11:38:41 +0930

    set keys via env

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@ $app->get('/declined/', function(Request $request) use($app) {
     return $app['twig']->render('declined.html');
 });
 $app->post('/', function(Request $request) use($app) {
-    \Stripe\Stripe::setApiKey(getenv('STRIPE_PRIVATE'));
+    \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET'));
     try {
         $charge = \Stripe\Charge::create(array(
             'amount' => ceil($request->get('amount') * 100),
